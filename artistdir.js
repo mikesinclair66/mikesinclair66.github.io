8d573bc63a654e6ba8c75c08ed6f588e8422d504
Michael Sinclair <michaelkevin.sinclair@gmail.com>
2020-01-29 09:27:00 -0800

    Finished Lab 3

--- artistdir.js
+++ artistdir.js
@@ -1,13 +1,25 @@
 var artists = new Array();
+var artistListDiv = document.getElementById("artistList");
 var count = 0;
 
 function insertArtist(imgLink, artistName, artistDesc){
-	artists[count] = {
+	//if an element is empty within the array, fill it without incrementing count
+	let index;
+	for(index = 0; index < count; index++)
+		if(artists[index] == undefined)
+			break;
+	
+	artists[index] = {
 		img: imgLink,
 		name: artistName,
-		desc: artistDesc
+		desc: artistDesc,
+		id: index
 	};
-	count++;
+	
+	if(index == count)
+		count++;
+	
+	return index;
 }
 
 insertArtist("https://randomuser.me/api/portraits/med/women/1.jpg", "Jessica Shen", "Lives in Asia");
@@ -21,11 +33,22 @@ function getArtistContainer(artist){
 	let imgTag = document.createElement("img");
 	imgTag.src = artist.img;
 	
+	let t = document.createTextNode(artist.name);
+	
 	let strongTag = document.createElement("strong");
-	strongTag.innerHTML = artist.name;
+	strongTag.appendChild(t);
+	
+	t = document.createTextNode(artist.desc);
 	
 	let pTag = document.createElement("p");
-	pTag.innerHTML = artist.desc;
+	pTag.appendChild(t);
+	
+	let inpTag = document.createElement("input");
+	inpTag.className = "delButton";
+	inpTag.type = "button";
+	inpTag.value = "Delete";
+	inpTag.onclick = function(){removeArtist(artist.id);};
+	inpTag.name = artist.id;
 	
 	let infoDiv = document.createElement("div");
 	infoDiv.appendChild(strongTag);
@@ -35,6 +58,7 @@ function getArtistContainer(artist){
 	let divTag = document.createElement("div");
 	divTag.appendChild(imgTag);
 	divTag.appendChild(infoDiv);
+	divTag.appendChild(inpTag);
 	divTag.className = "artistContainer";
 	
 	return divTag;
@@ -47,7 +71,87 @@ function printArtists(limit){
 		limit = count;
 	
 	for(let i = 0; i < limit; i++)
-		document.body.appendChild(getArtistContainer(artists[i]));
+		if(artists[i] != undefined)
+			artistListDiv.appendChild(getArtistContainer(artists[i]));
 }
 
 printArtists(count);
+
+var addArtistDiv = document.getElementById("addArtistDirectory");
+var addArtistToggled = false;
+//executed when add artist button is pressed
+function toggleAddArtist(){
+	if(!addArtistToggled){
+		addArtistToggled = true;
+		let artists = document.getElementsByClassName("artistContainer");
+		for(e of artists)
+			e.style.display = "none";
+		addArtistDiv.style.display = "block";
+	} else {
+		addArtistToggled = false;
+		let artists = document.getElementsByClassName("artistContainer");
+		for(e of artists)
+			e.style.display = "block";
+		addArtistDiv.style.display = "none";
+	}
+}
+
+var addInp = document.getElementById("addInp");
+
+//executed when add button is pressed
+function addArtist(){
+	//get the input values
+	let artistName = document.getElementById("artistNameInp");
+	let artistDesc = document.getElementById("artistDescInp");
+	let imgUrl = document.getElementById("imgUrlInp");
+	
+	let valEmpty = false;
+	
+	if(artistName.value == ""){
+		printError(artistName, "You must enter the artist's name.");
+		valEmpty = true;
+	} if(artistDesc.value == ""){
+		printError(artistDesc, "You must enter a description.");
+		valEmpty = true;
+	} if(imgUrl.value == ""){
+		printError(imgUrl, "You must enter an image url.");
+		valEmpty = true;
+	}
+	
+	//if all values have been filled, continue
+	if(!valEmpty){
+		let index = insertArtist(imgUrl.value, artistName.value, artistDesc.value);
+		artistListDiv.appendChild(getArtistContainer(artists[index]));
+		
+		//reset values
+		artistName.placeholder = "Artist name";
+		artistName.value = "";
+		artistDesc.placeholder = "About artist";
+		artistDesc.value = "";
+		imgUrl.placeholder = "Image url";
+		imgUrl.value = "";
+		addInp.style.border = "none";
+		
+		toggleAddArtist();
+	}
+}
+
+//prints error into the div
+function printError(input, msg){
+	input.value = "";
+	input.placeholder = msg;
+	addInp.style.border = "solid 1px red";
+}
+
+function removeArtist(id){
+	let containers = document.getElementsByClassName("artistContainer");
+	let delButtons = document.getElementsByClassName("delButton");
+	
+	for(let i = 0; i < delButtons.length; i++)
+		if(Number(delButtons[i].name) == id){
+			containers[i].remove();
+			artists[i] = undefined;
+		}
+}
+
+addInp.onclick = addArtist;
