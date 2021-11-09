36a172a901ca53602d351567af5c40ba21b3ee1e
Michael Sinclair <michaelkevin.sinclair@gmail.com>
2021-11-08 20:40:43 -0800

    Added case study 3

--- scripts/main.js
+++ scripts/main.js
@@ -49,4 +49,5 @@ async function revertToDir(){
 
 var docLinks = document.getElementsByClassName("doc_link");
 docLinks[0].onclick = () => displayDoc("Case_Study_Kingdom_Rush.pdf");
+docLinks[1].onclick = () => displayDoc("Case_Study_Polytopia.pdf");
 document.getElementsByClassName("back_link")[0].onclick = () => revertToDir();
